bcba5f5dcd3169fc42805a2f655c4e01cbfb2ae6
jnanaprakash <prakash143.thinkspace@gmail.com>
2023-08-28 20:41:31 +0530

    Contact us card component implemented

--- home-care/src/components/pages/ContactUs.js
+++ home-care/src/components/pages/ContactUs.js
@@ -30,64 +30,67 @@ const ContactUs = () => {
                     </div>
                 </div>
 
-                <div className="container">
 
+                <div className="container">
                     <div className="row">
-                        <h2>What can we help you with today?</h2>
-                        <div className="col-md-7">
-                            <div className="call-wrap">
-                                <img src={Cell} alt="" />
-                                <div>
-                                    <p>Call Us</p>
-                                    <a href="tell:9888559955" >9888 559955</a>
+                        <div className="col-md-12">
+                            <h2>What can we help you with today?</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="container call-wrap">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="card " >
+                                <div className="img-wrapper">
+                                    <img src={Cell} alt="baby care" />
+                                    <p>24/7 Service</p>
+                                    <h3>Call Us</h3>
                                 </div>
-                            </div>
-                            <div className="call-wrap">
-                                <img src={watsapp} alt="" />
-                                <div>
-                                    <p>WhatsApp Us At</p>
-                                    <a href="tell:9888559955" >9888 559955</a>
-                                </div>
-                            </div>
-                            <div className="call-wrap">
-                                <img src={Email} alt="" />
-                                <div>
-                                    <p>Write to Us</p>
-                                    <a href="mailto:jenceocustomercare@gmail.com" >jenceocustomercare@gmail.com</a>
+                                <div className="card-body">
+                                    <h4>  <a href="tell:9888559955" >9888 559955</a></h4>
                                 </div>
                             </div>
 
                         </div>
-                        <div className="col-md-5 sms">
-                            <div className="call-wrap">
-                                <img src={Location} alt="Location" />
-                                <div>
-                                    <h3><strong>Address</strong></h3>
-                                    <p>#2-3-381, Road No. 4,<br />
-                                        Sainagar Colony, Alkapuri X Road, <br />
-                                        Nagole, Hyderabad,  Telangana  (India), <br />Pin - 500 068,
-                                   </p>
-
+                        <div className="col-md-4">
+                            <div className="card " >
+                                <div className="img-wrapper">
+                                    <img src={Email} alt="email" />
+                                    <p>Droop a line</p>
+                                    <h3>Mail Us</h3>
                                 </div>
+                                <div className="card-body">
+                                    <h4>  <a href="mailto:jenceocustomercare@gmail.com" >jenceocustomercare@gmail.com</a></h4>
+                                </div>
+
                             </div>
+
+                        </div>
+                        <div className="col-md-4">
+                            <div className="card " >
+                                <div className="img-wrapper">
+                                    <img src={Location} alt="email" />
+                                    <p>Location</p>
+                                    <h3>Visit Us</h3>
+                                </div>
+                                <div className="card-body">
+                                    <p>#2-3-381, Road No. 4,
+                                        Sainagar Colony, Alkapuri X Road,
+                                        Nagole, Hyderabad,  Telangana  (India), Pin - 500 068,
+                                   </p>
+                                </div>
+
+                            </div>
+
                         </div>
 
                     </div>
                 </div>
 
-                <SendMessage/>
+                <SendMessage />
 
-                {/* <div className="container map">
 
-                    <h2>Location Map</h2>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876.580272636336!2d79.34547557661278!3d13.683267386701498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4d4b19d3f65f0b%3A0x9ceb617526255701!2sTIRUPATI%20BALAJI%20TEMPLE!5e0!3m2!1sen!2sin!4v1690853144756!5m2!1sen!2sin" width="600" height="450" ></iframe>
-
-                        </div>
-                    </div>
-
-                </div> */}
 
             </section>
 
